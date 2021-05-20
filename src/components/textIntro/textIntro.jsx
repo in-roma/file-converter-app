2bb625cf8351a1ebc12e0587efb02dad751970c9
Romain Grandjean <romain.grandjean@outlook.com>
2021-05-20 12:10:21 +0200

    test fix on mobile

--- src/components/textIntro/textIntro.jsx
+++ src/components/textIntro/textIntro.jsx
@@ -9,7 +9,7 @@ export default function TextIntro() {
 				You can create up to 9 categories and rename them (28 characters
 				maximum). For each category, you can create a maximum of 10
 				questions with a maximum of 4 options. The data is stored
-				locally and if you wish to start a new quiz, you can do so by
+				locally and if you wish to start a new trivia, you can do so by
 				clicking on the "Reset Trivia" button.
 			</span>
 		</div>
